c803521f93f4c970d9ffac7fd3759051c11d34ef
Michael Prieto <prieto4e@gmail.com>
2021-07-18 11:31:56 -0500

    clean js

--- 4-exchange-rate-calculator/script.js
+++ 4-exchange-rate-calculator/script.js
@@ -1,29 +1,23 @@
-const currencyEl_one = document.getElementById("currency-one");
-const amountEl_one = document.getElementById("amount-one");
-const currencyEl_two = document.getElementById("currency-two");
-const amountEl_two = document.getElementById("amount-two");
-const rateEl = document.getElementById("rate");
-const swap = document.getElementById("swap");
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
 
 // Fetch exchange rates and update the DOM
 function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
-  //const amount_one = amountEl_one.value;
-  //let amount_two = amountEl_two.value;
-  //amount_two = amount_one * rate;
-  
+
   fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
-    .then(res => res.json())
-    .then(data => {
-      //console.log(data);
+    .then((res) => res.json())
+    .then((data) => {
       const rate = data.rates[currency_two];
-      //console.log(rate);
 
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
       amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
     });
-    
 }
 
 // Event listeners
@@ -37,8 +31,6 @@ swap.addEventListener('click', () => {
   currencyEl_one.value = currencyEl_two.value;
   currencyEl_two.value = temp;
   calculate();
-})
-
+});
 
 calculate();
-
